57fb9da56d1252e150e0c94215119b03f594d70d
Thybow <tmilville.pro@gmail.com>
2023-09-04 20:21:00 +0200

    improvement - add a space before and after operators

--- script.js
+++ script.js
@@ -11,19 +11,19 @@ class calculator {
   calculate() {
     this.inputs.forEach((input) => {
       input.addEventListener("click", (e) => {
-        if(e.target.value == "=") {
-          if(this.inputDisplay.value != "") {
+        if (e.target.value == "=") {
+          if (this.inputDisplay.value != "") {
             this.inputDisplay.value = parseFloat(eval(this.inputDisplay.value).toFixed(2));
           } else {
             this.inputDisplay.value = "0";
           }
         } else {
-          switch(input.value) {
+          switch (input.value) {
             case "AC":
               this.inputDisplay.value = "0";
               break;
             case "DE":
-              if(this.inputDisplay.value.length == 1) {
+              if (this.inputDisplay.value.length == 1) {
                 this.inputDisplay.value = "0";
               } else {
                 this.inputDisplay.value = this.inputDisplay.value.substring(0, this.inputDisplay.value.length - 1);
@@ -32,16 +32,21 @@ class calculator {
             case ".":
               // Split the input by operators and check if the last number has a decimal
               let splitInput = this.inputDisplay.value.split(/[\+\-\*\/]/);
-              if(splitInput[splitInput.length - 1].indexOf(".") == -1) {
+              if (splitInput[splitInput.length - 1].indexOf(".") == -1) {
                 this.inputDisplay.value += input.value;
               }
               break;
             default:
               // Remove the 0 if it is the first number
-              if(this.inputDisplay.value == "0") {
+              if (this.inputDisplay.value == "0") {
                 this.inputDisplay.value = "";
               }
-              this.inputDisplay.value += input.value;
+              // Add a space before and after the operator
+              if (input.value.match(/[\+\-\*\/]/)) {
+                this.inputDisplay.value += " " + input.value + " ";
+              } else {
+                this.inputDisplay.value += input.value;
+              }
               break;
           }
         }
